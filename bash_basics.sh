e027b73936cb8f62246ce26a97a790da0a7ca50c
SHASHANKADASH <shibutom216@gmail.com>
2025-07-02 11:37:17 +0100

    Updated bash basics file

--- bash_basics.sh
+++ bash_basics.sh
@@ -25,3 +25,29 @@ echo "user's age is $age"
 # $0 has a special usage. It refers to the file itself
 echo "The name of the file is $0"
 # It can also be used to remove the file inside the script using rm -f $0
+
+# Arrays are defined in bash in below format
+list=("1" "2" "3")
+# To access the array elements
+echo ${list[1]}
+# To print all the elements
+echo ${list[@]}
+# To get the size of the array
+echo ${#list[@]}
+
+# Printing a range of elements
+array=(1 2 3 4 5)
+# Print from index 1 to 3
+echo ${array[@]:1:3}
+# Print from index 3
+echo ${array[@]:3}
+
+# In the same way we can slice string as well
+# Syntax : ${string:start:length}
+# Thing to note here is the last param denotes max chars to extract not the ending index unlike python. If we give a number greater than the
+# length of the string, it won't give an error. It will print the number of charcters possible to print from the string
+str="abcdefgh"
+# Extract from index 0, maximum 4 characters
+echo ${str:0:4}
+# Extract from index 0 to the end
+echo ${str:0}
